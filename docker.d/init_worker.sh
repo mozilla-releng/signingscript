7d3e4ecbcfc01fcbdc09d74cde274043c295306f
Rok Garbas <rok@garbas.si>
2019-08-08 18:02:15 +0200

    some more fixes

--- docker.d/init_worker.sh
+++ docker.d/init_worker.sh
@@ -6,8 +6,6 @@ set -e
 #
 test $CONFIG_DIR
 test $COT_PRODUCT
-test $DATADOG_HOST
-test $DATADOG_PORT
 test $GPG_PUBKEY
 test $PROJECT_NAME
 test $PUBLIC_IP
@@ -17,8 +15,9 @@ test $PUBLIC_IP
 #export HFSPLUS_PATH=...
 #export ZIPALIGN_PATH=...
 
-export SIGNTOOL_PATH="/app/signtool"
-export SSL_CERT_PATH="$(dirname $0)/../signingscript/data/host.cert"
+export PASSWORDS_PATH=$CONFIG_DIR/passwords.json
+export SIGNTOOL_PATH="/app/bin/signtool"
+export SSL_CERT_PATH="/app/signingscript/data/host.cert"
 export GPG_PUBKEY_PATH=$CONFIG_DIR/gpg_pubkey
 export WIDEVINE_CERT_PATH=$CONFIG_DIR/widevine.crt
 
@@ -27,11 +26,14 @@ echo $GPG_PUBKEY | base64 -d > $GPG_PUBKEY_PATH
 case $COT_PRODUCT in
   firefox)
     test $WIDEVINE_CERT
+
     echo $WIDEVINE_CERT | base64 -d > $WIDEVINE_CERT_PATH
-    export TASKCLUSTER_SCOPE_PREFIX="project:releng:${PROJECT_NAME}script:"
     ;;
   thunderbird)
-    export TASKCLUSTER_SCOPE_PREFIX="project:comm:thunderbird:releng:${PROJECT_NAME}script:"
+    ;;
+  mobile)
+    ;;
+  application-services)
     ;;
   *)
     exit 1
