713522f5fd5bc2c7098d93ea88a9adef0c479b4e
Rail Aliiev <rail@mozilla.com>
2019-08-29 21:00:00 -0400

    Add widevine support

--- docker.d/init_worker.sh
+++ docker.d/init_worker.sh
@@ -7,28 +7,27 @@ set -e
 test $CONFIG_DIR
 test $CONFIG_LOADER
 test $COT_PRODUCT
-test $GPG_PUBKEY
 test $PROJECT_NAME
 test $PUBLIC_IP
 test $TEMPLATE_DIR
+test $WIDEVINE_MODULE
 
-export DMG_PATH=/app/files/dmg
-export HFSPLUS_PATH=/app/files/hfsplus
+FILES_DIR=/app/files
+export DMG_PATH=$FILES_DIR/dmg
+export HFSPLUS_PATH=$FILES_DIR/hfsplus
 export ZIPALIGN_PATH=/usr/bin/zipalign
 
 export PASSWORDS_PATH=$CONFIG_DIR/passwords.json
 export SIGNTOOL_PATH="/app/bin/signtool"
 export SSL_CERT_PATH="/app/signingscript/data/host.cert"
-export GPG_PUBKEY_PATH=$CONFIG_DIR/gpg_pubkey
 export WIDEVINE_CERT_PATH=$CONFIG_DIR/widevine.crt
 
-echo $GPG_PUBKEY | base64 -d > $GPG_PUBKEY_PATH
+WIDEVINE_MODULE_PATH=/app/widevine-0.1.0-py3-none-any.whl
+echo $WIDEVINE_MODULE | base64 -d > $WIDEVINE_MODULE_PATH
+/app/bin/pip install -U $WIDEVINE_MODULE_PATH
 
 case $COT_PRODUCT in
   firefox)
-    test $WIDEVINE_CERT
-
-    echo $WIDEVINE_CERT | base64 -d > $WIDEVINE_CERT_PATH
     ;;
   thunderbird)
     ;;
@@ -38,6 +37,17 @@ case $COT_PRODUCT in
     ;;
   *)
     exit 1
+    ;;
+esac
+
+case $ENV in
+  prod)
+    export GPG_PUBKEY_PATH=$FILES_DIR/KEY_prod
+    export WIDEVINE_CERT_PATH=$FILES_DIR/widevine_prod.crt
+    ;;
+  *)
+    export GPG_PUBKEY_PATH=$FILES_DIR/KEY_dep
+    export WIDEVINE_CERT_PATH=$FILES_DIR/widevine_dep.crt
     ;;
 esac
 
@@ -104,8 +114,6 @@ case $ENV in
         test $AUTOGRAPH_FENNEC_RELEASE_PASSWORD
         test $AUTOGRAPH_FENNEC_RELEASE_USERNAME
         test $AUTOGRAPH_GPG_PASSWORD
-        test $AUTOGRAPH_GPG_PASSWORD
-        test $AUTOGRAPH_GPG_USERNAME
         test $AUTOGRAPH_GPG_USERNAME
         test $AUTOGRAPH_LANGPACK_PASSWORD
         test $AUTOGRAPH_LANGPACK_USERNAME
